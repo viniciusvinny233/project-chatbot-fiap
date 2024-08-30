73a7fef803536b5fb1f72a88072a114202f909bc
Ana "Mytka" Vitória <111380860+mytkadev@users.noreply.github.com>
2024-08-29 22:55:47 -0300

    mudança chamados frequentes

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -68,13 +68,13 @@ const Faq: React.FC = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#1a1a1a] text-white p-4 md:p-13">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#1a1a1a] text-white p-4 md:p-13 bg-custom-gradient">
       <div className="w-full max-w-7xl">
-        <div className='flex flex-col justify-center md:flex-row md:justify-between items-center'>
-          <p className="mt-4 text-gray-400 w-full md:max-w-56 md:text-start text-center">
+        <div className='justify-center md:flex-row md:justify-between items-center'>
+          <h1 className="text-4xl font-bold bg-gradient-to-r from-[#82E0F5] to-[#E27696] text-transparent bg-clip-text mt-2 md:text-center text-center md:mt-3">CHAMADOS FREQUENTES</h1>
+          <p className="mt-4 text-gray-400 w-full  text-center">
             Caso sua dúvida não esteja aqui é só chamar nosso chatbot, Catty.
           </p>
-          <h1 className="text-4xl font-bold text-[#77C1C6] mt-2 md:text-center text-center md:mt-3">PERGUNTAS <br />FREQUENTES</h1>
         </div>
 
         <div className="mt-8 max-h-[36rem] md:max-h-[42rem] overflow-y-auto space-y-5 scrollbar-thin scrollbar-thumb-rounded-lg scrollbar-thumb-[#77C1C6] scrollbar-track-[#2a2a2a] pr-4">
