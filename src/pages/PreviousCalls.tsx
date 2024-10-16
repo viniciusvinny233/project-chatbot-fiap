b48716544d90abbffd5d66f583f3ab73bacec32d
Ana "Mytka" Vitória <111380860+mytkadev@users.noreply.github.com>
2024-10-16 11:26:57 -0300

    Update PreviousCalls.tsx

--- src/pages/PreviousCalls.tsx
+++ src/pages/PreviousCalls.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AvatarChamados from '../assets/images/Avatar.png';
+import AvatarChamados from '../assets/images/avatar.png';
 
 interface Ticket {
   idTicket: string;
