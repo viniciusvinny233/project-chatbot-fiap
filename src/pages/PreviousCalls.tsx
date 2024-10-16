28d6fb5dfa55aa7ceb3306f45a533b8e983580d4
Ana "Mytka" Vitória <111380860+mytkadev@users.noreply.github.com>
2024-10-16 11:21:07 -0300

    bug integração previouscalls

--- src/pages/PreviousCalls.tsx
+++ src/pages/PreviousCalls.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import AvatarChamados from '../assets/images/Avatar.png';
+import { useNavigate } from 'react-router-dom';
+import AvatarChamados from '../assets/images/avatar.png';
 
-// Definir a interface dos tickets
 interface Ticket {
   idTicket: string;
   numeroTicket: string;
@@ -17,11 +17,10 @@ const PreviousCalls: React.FC = () => {
   const [calls, setCalls] = useState<Ticket[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
+  const navigate = useNavigate();
   const baseUrl = "https://softtek-api-5e04b4d63dfd.herokuapp.com/api/tickets";
-  const pageSize = 5; // Tamanho da página, ajustável conforme necessário
+  const pageSize = 5;
 
-  // Função para converter timestamp Unix para data legível
   const formatUnixTimestampToDateTime = (unixTimestamp: number | null): string => {
     if (unixTimestamp === null) return 'N/A';
     const date = new Date(unixTimestamp);
@@ -36,7 +35,6 @@ const PreviousCalls: React.FC = () => {
     });
   };
 
-  // Função para buscar dados de todas as páginas da API
   const fetchAllPages = async () => {
     let currentPage = 1;
     let allTickets: Ticket[] = [];
@@ -57,19 +55,15 @@ const PreviousCalls: React.FC = () => {
 
         const data = await response.json();
 
-        // Log para verificar a resposta da API e depurar
-        console.log(`Página ${currentPage} de dados:`, data);
-
-        // Verifica se ainda há conteúdo
         if (data.content && data.content.length > 0) {
-          allTickets = allTickets.concat(data.content); // Adiciona os itens ao array
-          currentPage++; // Próxima página
+          allTickets = allTickets.concat(data.content);
+          currentPage++;
         } else {
-          hasMorePages = false; // Se não houver mais conteúdo, interrompe o loop
+          hasMorePages = false;
         }
       }
 
-      setCalls(allTickets); // Atualiza o estado com todos os tickets coletados
+      setCalls(allTickets);
       setIsLoading(false);
     } catch (error: any) {
       setError(error.message);
@@ -81,8 +75,12 @@ const PreviousCalls: React.FC = () => {
     fetchAllPages();
   }, []);
 
+  const handleRowClick = (idTicket: string) => {
+    navigate(`/ticket/${idTicket}`);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen bg-[#1a1a1a] text-white p-4 md:p-13 pb-0 md:pb-0">
+    <div className="flex flex-col items-center justify-between min-h-screen bg-[#1a1a1a] text-white p-4 md:p-13 pb-0">
       <div className="w-full max-w-7xl">
         <div className='flex flex-col justify-center md:flex-row items-center'>
           <h1 className="text-4xl font-bold bg-gradient-to-r from-[#82E0F5] to-[#E27696] text-transparent bg-clip-text mt-2 md:text-center text-center md:mt-3">
@@ -104,60 +102,14 @@ const PreviousCalls: React.FC = () => {
             <input
               type="text"
               placeholder="Buscar um chamado ou assunto"
-              className="p-3 h-16 bg-[#222222] rounded-md md:w-80 border min-w-64 border-[#363636] focus:outline-none focus:bg-[#1a1a1a]"
+              className="p-3 px-5 h-16 bg-[#222222] rounded-md md:w-80 border min-w-64 border-[#363636] focus:outline-none focus:bg-[#1a1a1a]"
             />
           </div>
         </div>
 
         <div className="scrollbar relative h-[47.2rem] mt-8 left-0 right-0 rounded-t-xl bg-[#2a2a2a] overflow-y-auto px-4 py-2 pb-2">
           {isLoading ? (
-            <p>Carregando dados...</p>
-          ) : error ? (
-            <p className="text-red-500">Erro: {error}</p>
-          ) : (
-            <table className="min-w-full text-sm text-left text-gray-300">
-              <thead className="text-base uppercase border-b text-gray-300">
-                <tr>
-                  <th scope="col" className="px-2 py-2 w-[10%]">ID</th>
-                  <th scope="col" className="px-2 py-2 w-[15%]">Abertura</th>
-                  <th scope="col" className="px-2 py-2 w-[35%]">Assunto</th>
-                  <th scope="col" className="px-2 py-2 w-[15%]">Atualização</th>
-                  <th scope="col" className="px-2 py-2 w-[15%]">Resolução</th>
-                  <th scope="col" className="px-2 py-2 w-[10%]">Encerramento</th>
-                  <th scope="col" className="px-2 py-2 w-[10%] text-center">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {calls.map((call, index) => (
-                  <tr key={index} className="border-b border-gray-700">
-                    <td className="px-2 py-2">{call.numeroTicket || 'N/A'}</td>
-                    <td className="px-2 py-2">{formatUnixTimestampToDateTime(call.dataAbertura)}</td>
-                    <td className="px-2 py-2">{call.descricaoTicket || 'N/A'}</td>
-                    <td className="px-2 py-2">{formatUnixTimestampToDateTime(call.dataAtualizacao)}</td>
-                    <td className="px-2 py-2">{formatUnixTimestampToDateTime(call.dataRelatorioResolvido)}</td>
-                    <td className="px-2 py-2">{formatUnixTimestampToDateTime(call.dataEncerramento)}</td>
-                    <td className="px-2 py-2 text-center">
-                      <span
-                        className={`inline-block py-1 px-3 rounded-xl font-bold text-white w-full text-center ${
-                          call.descricaoEstadoTicket === 'Resolvido'
-                            ? 'text-xs uppercase bg-lime-800 text-lime-400'
-                            : call.descricaoEstadoTicket === 'Em Andamento'
-                            ? 'text-xs  uppercase bg-yellow-800 text-yellow-200'
-                            : 'text-xs  uppercase bg-zinc-700 text-zinc-400'
-                        }`}
-                      >
-                        {call.descricaoEstadoTicket || 'N/A'}
-                      </span>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PreviousCalls;
+            <div role="status">
+              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12
