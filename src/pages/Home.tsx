1e0ad33d9a360f4b3d373d5ed8028e491f51221c
Ana "Mytka" Vitória <111380860+mytkadev@users.noreply.github.com>
2024-10-21 17:17:06 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,215 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { containerConstants, teamMembers } from "../assets/home/constants";
+import logosCatech from "../assets/home/images/LogosChallenge.png";
+import heroSectionImg from "../assets/home/images/HeroSectionImg.png";
+import firstSectionBgImg from "../assets/home/images/firstSectionBgImg.png";
+import logosCatechBlack from "../assets/home/images/LogosChallengeBlack.png";
+import sixthContainerFirstSectionBg from "../assets/home/images/sixthSectionFirstContainerBg.webp";
+import sixthContainerSecondSectionBg from "../assets/home/images/sixthSectionSecondContainerBg.webp";
+import sixthContainerSecondSectionImg from "../assets/home/images/fifthSectionSecondContainerImg.png";
+import sixthContainerFirstSectionImg from "../assets/home/images/fifthSectionfirstContainerImg.png";
+import github from "../assets/home/images/GitHub.png"
+import linkedin from "../assets/home/images/LinkedIn.png"
 
-import BotIcon from '../assets/images/1.png';
-import FiapIcon from '../assets/images/3.png';
-import LogoRosaIcon from '../assets/images/logo-rosa.png';
+const scrollToSection = (id: string) => {
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+const ButtonLink: React.FC<{ to: string, text: string }> = ({ to, text }) => (
+  <Link to={to}>
+    <div className="bg-[#BFF3FF] py-1.5 px-4 rounded-full flex items-center text-black font-semibold shadow-shape gap-1">
+      {text}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-arrow-up-right w-4"
+      >
+        <path d="M7 7h10v10"/>
+        <path d="M7 17 17 7"/>
+      </svg>
+    </div>
+  </Link>
+);
+
+const TextLink: React.FC<{ href: string, text: string }> = ({ href, text }) => (
+  <a href={href} onClick={(e) => {
+    e.preventDefault();
+    scrollToSection('about');
+  }}>
+    <div className="flex items-center py-1 text-[#DFDFDF] space-x-1 cursor-pointer">
+      <span>{text}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-chevron-down w-4"
+      >
+        <path d="m6 9 6 6 6-6"/>
+      </svg>
+    </div>
+  </a>
+);
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen md:h-max items-center justify-between text-center px-4 md:px-8 lg:px-16 bg-custom-gradient">
-      <div className="flex flex-col items-center mb-12">
-        <div className="flex flex-col md:flex-row items-center mb-12 mt-3 justify-center">
-          <img src={FiapIcon} alt="Icon" className="w-[8em] h-auto md:w-[10em] md:h-[1.25em] mr-0 md:mr-4 mb-4 md:mb-0" />
-          <img src={LogoRosaIcon} alt="Icon" className="w-[2.1em] h-[2.15em]" />
-        </div>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-inter text-customBlue mb-3 leading-tight">
-          Bem vindo! <b>como posso ajudar você?</b>
+    <div className="font-inter antialiased flex flex-col justify-center items-center text-center">
+      <img src={logosCatech} alt="Logos Fiap, Softtek e Catech" className="w-40 mt-11 md:mt-16" />
+      <div className="px-7 flex flex-col items-center">
+        <h1 className="text-3xl sm:text-4xl md:text-7xl text-[#BFDDDF] leading-7 pt-4 md:pt-7">
+          Bem vindo à{" "}
+          <span className="bg-gradient-to-r font-semibold from-[#82E0F5] to-[#E27696] text-transparent bg-clip-text">Catech</span>!
+          <br />
+          Como posso ajudar você?
         </h1>
-        <p className="text-customText font-inter max-w-2xl md:max-w-3xl lg:max-w-5xl px-4 md:px-0 leading-relaxed">
-        Seu assistente técnico inteligente desenvolvido para oferecer suporte de forma amigável e eficiente. Combinamos tecnologia de ponta com uma experiência acolhedora. 
-        Conheça nossa gatinha assistente, Catty, e descubra como podemos facilitar seu trabalho.
+        <p className="text-[#ADADAD] font-light text-sm md:text-base pt-4 max-w-[30rem]">
+          Seu assistente técnico inteligente desenvolvido para oferecer suporte de forma amigável e eficiente.
         </p>
-        <div className="mt-5 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
-          <a href="https://drive.google.com/file/d/16EFh6DlUEKhbncXS_qePD6NtpaH1wMfg/view?usp=sharing" target='_blank'>
-          <button className="font-inter font-semibold w-[9.5rem] h-[3.75rem] bg-transparent border border-gradient text-[#D7E5E6] rounded-[0.9rem] hover:text-gray-400 ">
-            PDF Projeto
-          </button>
-          </a>
-          <a href="https://www.figma.com/design/cPqrECL5rbLyhfT9QExV10/Challenge-Softtek-%7C-Catech?node-id=2-2&t=f11sM7WzVzYyUiTg-1" target='_blank'>
-          <button className="font-inter font-semibold w-[9.5rem] h-[3.75rem] border border-gradient text-[#D7E5E6] rounded-[0.9rem] hover:text-gray-400">
-            Figma
-          </button>
-          </a>
-          <a href="https://www.youtube.com/watch?v=gD09yEANSOw" target='_blank'>
-          <button className="font-inter font-semibold w-[9.5rem] h-[3.75rem] border border-gradient text-[#D7E5E6] rounded-[0.9rem] hover:text-gray-400">
-            Pitch
-          </button>
-          </a>
-          <a href="https://github.com/viniciusvinny233/project-chatbot-fiap/tree/master" target='_blank'>
-          <button className="font-inter font-semibold w-[9.5rem] h-[3.75rem] bg-transparent border border-gradient text-[#D7E5E6] rounded-[0.9rem] hover:text-gray-400">
-            Frontend
-          </button>
-          </a>
-          <a href="https://github.com/matheusgbrt/ChallengeSoftTek" target='_blank'>
-          <button className="font-inter font-semibold w-[9.5rem] h-[3.75rem] bg-transparent border border-gradient text-[#D7E5E6] rounded-[0.9rem] hover:text-gray-400">
-            Backend
-          </button>
-          </a>
+      </div>
+      <div id="buttons" className="text-sm md:text-base my-6 flex flex-col sm:flex-row sm:space-x-4 space-y-2 sm:space-y-0 justify-center items-center">
+        <ButtonLink to="/chatbot" text="Visitar a aplicação" />
+        <TextLink href="#about" text="Sobre a Catech"/>
+      </div>
+      <img src={heroSectionImg} alt="Imagem de um computador com uma página da Catty sendo utilizada" className="max-h-[800px]"/>
+      <div className="mt-16 md:mt-32 px-7 flex flex-col justify-center items-center" id="about">
+        <h2 className="text-3xl md:text-6xl text-[#BFDDDF] leading-8 pt-4 md:pt-7">
+          Qual a origem da <span className="bg-gradient-to-r font-bold from-[#82E0F5] to-[#E27696] text-transparent bg-clip-text">Catech</span>?
+        </h2>
+        <p className="text-[#CFCFCF] text-sm md:text-base max-w-[60rem] pt-3">
+          A Catech foi desenvolvida como parte de um desafio da Softtek para a turma do segundo ano de Engenharia de Software da FIAP, visando aprimorar a eficiência no suporte técnico com a criação de um robô assistente integrado a tecnologias avançadas.
+        </p>
+        <div className="relative mt-9 rounded-2xl overflow-hidden md:max-w-6xl lg:mx-12">
+          <img src={firstSectionBgImg} alt="" className="h-[300px] sm:h-full object-cover w-full"/>
+          <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4">
+            <h3 className="text-[#222222] font-bold text-base md:text-xl xl:text-2xl leading-4 text-center px-6">
+              Conheça um pouco sobre nós pelo olhar da nossa robô, Catty
+            </h3>
+            <div className="w-full flex justify-center">
+              <iframe
+                className="w-3/4  sm:h-64 md:h-80 lg:h-96 xl:h-[480px] rounded-lg shadow-shape"
+                src="https://www.youtube.com/embed/gD09yEANSOw?si=aHdWPXAzvZlWuWXk&controls=0"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                referrerPolicy="strict-origin-when-cross-origin"
+              />
+            </div>
+
+
+            <img src={logosCatechBlack} alt="Logos Catech em preto" className="w-40"/>
+          </div>
         </div>
       </div>
-      <div className="flex justify-center mt-[-2rem] md:mt-[9.1rem] lg:mt-[4.1rem]">
-        <img src={BotIcon} alt="Icon" className="w-60 md:w-[30em] lg:w-[38em] h-auto" />
+      {containerConstants.map((container, index) => (
+        <div key={index} className="mt-12 md:mt-16 px-7 flex flex-col justify-center items-center">
+          <h2 className="text-3xl md:text-6xl text-[#BFDDDF] leading-8 pt-4 md:pt-7 max-w-[60rem] text-balance">
+            {container.title}
+          </h2>
+          <p className="text-[#CFCFCF] text-sm md:text-base max-w-[60rem] pt-3">
+            {container.desc}
+          </p>
+          <div className="relative mt-8 rounded-2xl overflow-hidden md:max-w-6xl lg:mx-12">
+            <img src={container.bgImg} alt="" className="h-[440px] sm:h-full object-cover w-full shadow-shape"/>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <video src={container.containerVideo} autoPlay loop muted className="hidden sm:flex sm:w-5/6 lg:w-4/5 rounded-xl "/>
+              <video src={container.mobileContainerVideo} autoPlay loop muted className="sm:hidden rounded-xl w-3/4"/>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div className="mt-12 md:mt-16 flex flex-col items-center px-8">
+        <h2 className="text-3xl md:text-6xl text-[#BFDDDF] leading-8 pt-4 md:pt-7 max-w-[60rem]">
+          Vá além como administrador
+        </h2>
+        {/* Primeira Seção */}
+        <section className="mt-7 text-start flex flex-col sm:flex-row max-w-[1129px]">
+          <div className="shadow-shape border border-[#5B5B5B] rounded-t-2xl px-6 py-5 sm:px-14 sm:py-11 h-[17rem] sm:w-1/2 sm:h-[380px] sm:rounded-t-none sm:rounded-l-3xl sm:rounded-tl-3xl">
+            <h3 className="text-2xl md:text-3xl text-[#BFDDDF] leading-6">
+              Visualize todos os chamados e os status de cada assistente técnico
+            </h3>
+            <p className="text-xs md:text-sm text-[#CFCFCF] mt-3">
+              Na página de chamados do administrador, você tem acesso a uma visão geral de todos os assistentes e todos os chamados, inclusive os que já foram resolvidos e os que estão em andamento.
+            </p>
+          </div>
+          <div className="shadow-shape rounded-b-2xl sm:w-1/2 sm:rounded-b-none sm:rounded-r-3xl relative overflow-hidden h-[17rem] sm:h-[380px] border border-[#5B5B5B] sm:border-l-0 ">
+            <img 
+              src={sixthContainerFirstSectionBg} 
+              alt="" 
+              className="object-cover h-full w-full rounded-b-2xl sm:h-[380px] sm:rounded-b-none sm:rounded-r-3xl"
+            />
+            <img 
+              src={sixthContainerFirstSectionImg} 
+              alt="" 
+              className="absolute top-12 left-12 md:top-20 md:left-20 rounded-xl border border-[#5B5B5B] shadow-shape"
+              style={{ width: 'auto', height: 'auto', maxWidth: '700px', maxHeight: '420px' }}
+            />
+          </div>
+        </section>
+        {/* Segunda Seção */}
+        <section className="mt-7 text-start flex flex-col sm:flex-row max-w-[1129px]">
+          <div className="shadow-shape border border-[#5B5B5B] rounded-t-2xl px-6 py-5 sm:px-14 sm:py-11 h-[17rem] sm:w-1/2 sm:h-[380px] sm:rounded-t-none sm:rounded-l-3xl sm:rounded-tl-3xl">
+            <h3 className="text-2xl md:text-3xl text-[#BFDDDF] leading-6">
+              Tenha acesso a relatórios de chamados, produtividade individual e da equipe
+            </h3>
+            <p className="text-xs md:text-sm text-[#CFCFCF] mt-3">
+              Como administrador, você tem a opção dentro da página de chamados de criar um relatório tanto da equipe inteira quanto individualmente. Saiba a produtividade dos assistentes técnicos, os chamados que acontecem com maior frequência e mais dados que se conectam com nossa plataforma.
+            </p>
+          </div>
+          <div className="shadow-shape rounded-b-2xl sm:w-1/2 sm:rounded-b-none sm:rounded-r-3xl relative overflow-hidden h-[17rem] sm:h-[380px] border border-[#5B5B5B] sm:border-l-0">
+            <img 
+              src={sixthContainerSecondSectionBg} 
+              alt="" 
+              className="object-cover h-full w-full rounded-b-2xl sm:h-[380px] sm:rounded-b-none sm:rounded-r-3xl"
+            />
+            <img 
+              src={sixthContainerSecondSectionImg} 
+              alt="" 
+              className="absolute top-12 left-12 md:top-20 md:left-20 rounded-xl border border-[#5B5B5B] shadow-shape"
+              style={{ width: 'auto', height: 'auto', maxWidth: '700px', maxHeight: '420px' }}
+            />
+          </div>
+        </section>
       </div>
+      <div className="mt-12 md:mt-16 flex flex-col items-center px-8 max-w-[1300px]">
+  <h2 className="text-3xl md:text-6xl text-[#BFDDDF] leading-8 pt-4 md:pt-7 max-w-[60rem]">
+    Conheça nossa equipe:
+  </h2>
+  <div className="w-full flex justify-center mt-14 mb-20">
+    <div className="flex flex-wrap justify-center gap-x-12 gap-y-8">
+      {teamMembers.map((team, index) => (
+        <div key={index} className="">
+          <img src={team.icon} alt="" className="w-full h-52 object-cover shadow-shape rounded-md "/>
+          <div className="flex flex-col items-start min-w-[283px] max-w-[283px] text-[#CFCFCF] mt-3">
+          <p className="text-lg font-extrabold leading-tight">{team.role}</p>
+          <p className="text-sm">{team.name}</p>
+          <div className="flex space-x-2 mt-2">
+            <a href={team.linkedin}><img src={linkedin} alt="LinkedIn logo" className="w-5"/></a>
+            <a href={team.github}><img src={github} alt="GitHub logo" className="w-5"/></a>
+          </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+</div>
+
+
     </div>
   );
 };
